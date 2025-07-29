2aae861ea8257d62f17607b54e3ad85a65f8ad14
Gabriel Lopes Agiova <glopesagiova@gmail.com>
2025-07-29 18:17:14 -0300

    feat: add action button add product

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, MatButtonModule, RouterLink],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
