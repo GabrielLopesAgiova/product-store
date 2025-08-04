c96497836d009ce1fea0f91997d77b1d02cfdd8a
Gabriel Lopes Agiova <glopesagiova@gmail.com>
2025-08-03 21:20:58 -0300

    feat: recover data edit page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
-import { Routes } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
 import { CreateComponent } from './features/create/create.component';
+import { Observable } from 'rxjs';
+import { ProductsService } from './shared/services/products.service';
+import { inject } from '@angular/core';
 
 export const routes: Routes = [
     {
@@ -13,7 +16,13 @@ export const routes: Routes = [
             .then(module => module.CreateComponent)
     },
     {
-        path: 'edit-product',
+        path: 'edit-product/:id',
+        resolve: {
+            product: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+                const productsService = inject(ProductsService);
+                return productsService.get(route.paramMap.get('id') as string);
+            }
+        },
         loadComponent: () => import('./features/edit/edit.component')
             .then(module => module.EditComponent)
     }
