40d9b20966fb6bcbec6a8307c8cb8a2e196ceda2
Gabriel Lopes Agiova <glopesagiova@gmail.com>
2025-08-07 20:27:49 -0300

    refactor: add resolver files to application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,13 @@
-import { ActivatedRoute, ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
-import { CreateComponent } from './features/create/create.component';
-import { Observable } from 'rxjs';
-import { ProductsService } from './shared/services/products.service';
-import { inject } from '@angular/core';
+import { getProducts } from './shared/resolvers/get-products.resolver';
+import { getProduct } from './shared/resolvers/get-product.resolver';
 
 export const routes: Routes = [
     {
         path: '',
         resolve: {
-            products: () => {
-               const productsService = inject(ProductsService);
-               return productsService.getAll()
-            }
+            products: getProducts
         },
         component: ListComponent
     },
@@ -24,10 +19,7 @@ export const routes: Routes = [
     {
         path: 'edit-product/:id',
         resolve: {
-            product: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-                const productsService = inject(ProductsService);
-                return productsService.get(route.paramMap.get('id') as string);
-            }
+            product: getProduct
         },
         loadComponent: () => import('./features/edit/edit.component')
             .then(module => module.EditComponent)
